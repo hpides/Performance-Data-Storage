a01123730c75dbf656f2b972d635285f299db573
Eric Ackermann <eric.ackermann@student.hpi.de>
2020-02-12 18:37:58 +0100

    Run curl inside container

--- Docker/dockerTest.sh
+++ Docker/dockerTest.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./buildAndRun.sh -d || exit 1
 sleep 30
-curl http://localhost:8080/tests/running || (docker logs docker_users_1 && exit 1)
+docker exec -it docker_users_1 curl http://localhost:6080/tests/running || (docker logs docker_users_1 && exit 1)
