ecf6244b8ba33be1c54c39e32395c43eacb43450
Eric Ackermann <eric.ackermann@student.hpi.de>
2020-02-12 18:24:31 +0100

    Install curl and show logs if failed

--- Docker/dockerTest.sh
+++ Docker/dockerTest.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 ./buildAndRun.sh -d || exit 1
 sleep 30
-curl http://localhost:8080/tests/running
-exit $?
+curl http://localhost:8080/tests/running || (docker logs docker_users_1 && exit 1)
