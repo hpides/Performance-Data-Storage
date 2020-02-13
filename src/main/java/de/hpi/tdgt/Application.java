35d6078dc10b62d2d0efc470210b40cc59f05c73
Eric Ackermann <eric.ackermann@student.hpi.de>
2020-02-13 14:29:01 +0100

    Add CORS rule for frontend

--- src/main/java/de/hpi/tdgt/Application.java
+++ src/main/java/de/hpi/tdgt/Application.java
@@ -3,12 +3,23 @@ package de.hpi.tdgt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
