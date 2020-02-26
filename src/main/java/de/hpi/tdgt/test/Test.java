5ffc2658687b14baca559fb18e65cfdf1f03db48
Eric Ackermann <eric.ackermann@student.hpi.de>
2020-02-26 15:25:31 +0100

    Do not export times and assertions with test (cyclic)

--- src/main/java/de/hpi/tdgt/test/Test.java
+++ src/main/java/de/hpi/tdgt/test/Test.java
@@ -1,5 +1,6 @@
 package de.hpi.tdgt.test;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
@@ -21,12 +22,16 @@ public class Test {
     private String testConfig;
 
     private boolean isActive = true;
+
+    //do not export them as JSON, else there is a cycle
     //workaround for multiplebagfetchexception, see https://stackoverflow.com/questions/17566304/multiple-fetches-with-eager-type-in-hibernate-with-jpa
     @Fetch(FetchMode.SELECT)
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE, orphanRemoval = true, mappedBy = "test")
+    @JsonIgnore
     private List<ReportedTime> times;
 
     @Fetch(FetchMode.SELECT)
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE, orphanRemoval = true, mappedBy = "test")
+    @JsonIgnore
     private List<ReportedAssertion> assertions;
 }
